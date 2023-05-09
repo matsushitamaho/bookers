395b85055fc9062acd2405f61fd69d52fd25d754
mathushitamaho <132318410+matsushitamaho@users.noreply.github.com>
2023-05-09 12:50:40 +0000

    topページのレイアウト終了、一覧・編集・削除のエラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,17 @@
 class BooksController < ApplicationController
   def index
+    @boo=Book.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to'/books'
   end
 end
