934f7d99683dc620a1b179c76839d031d42e2046
mathushitamaho <132318410+matsushitamaho@users.noreply.github.com>
2023-05-10 15:40:41 +0000

    top,index 完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,18 @@
 class BooksController < ApplicationController
   def index
-    @books=Book.new
+    @books=Book.all
+    @book=Book.new
+  end
+  
+  def create
+    @book=Book.new(book_params)
+    if @book.save
+      flash[:notice]="Book was successflly created."
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all.order(id:asc)
+      render :index
+    end
   end
 
   def show
@@ -14,5 +26,17 @@ class BooksController < ApplicationController
     book=Book.find(params[:id])
     book.destroy
     redirect_to'/books/index'
+  end
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice]="Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
