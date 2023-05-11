2e8f8c80fad26605d7a205a1d6651435ebc7400a
mathushitamaho <132318410+matsushitamaho@users.noreply.github.com>
2023-05-11 12:12:56 +0000

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,17 +10,17 @@ class BooksController < ApplicationController
       flash[:notice]="Book was successflly created."
       redirect_to book_path(@book.id)
     else
-      #@books=Book.all
+      @books=Book.all
       render :index
     end
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book=Book.find(params[:id])
   end
 
   def edit
-    @book =Book.find(params[:id])
+    @book=Book.find(params[:id])
   end
 
   def destroy
